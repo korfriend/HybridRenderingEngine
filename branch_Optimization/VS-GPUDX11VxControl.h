8a53d381f0594c0dba48f418967805f569e1d384
korfriend <korfriend@gmail.com>
2020-07-14 15:52:15 +0900

    delete files

--- branch_Optimization/VS-GPUDX11VxControl.h
+++
@@ -1,9 +0,0 @@
-#include "VXEngineGlobalUnit.h"
-
-__vxstatic bool CheckIsAvailableParameters(SVXModuleParameters* pstModuleParameter);
-__vxstatic bool InitModule(SVXModuleParameters* pstModuleParameter);
-__vxstatic bool DoModule(SVXModuleParameters* pstModuleParameter);
-__vxstatic void DeInitModule(map<wstring, wstring>* pmapCustomParamters);
-__vxstatic double GetProgress();
-__vxstatic void InteropCustomWork(SVXModuleParameters* pstModuleParameter);
-__vxstatic void GetModuleSpecification(vector<wstring>* pvtrSpecification);
