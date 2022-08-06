96b2d57a0b0906a702bb3ff5a063301bceb5f047
동준 김 <korfriend@gmail.com>
2022-08-07 01:52:20 +0900

    fix picking error (add the np distance)

--- hlsl/dx_abuf/DxABuf.hlsl
+++ hlsl/dx_abuf/DxABuf.hlsl
@@ -168,7 +168,29 @@ void PICKING_A_BUFFER_FILL(__VS_OUT input)
 	int y_pick_ss = g_cbCamState.iSrCamDummy__1 >> 16;
 	if(x_pick_ss != tex2d_xy.x || y_pick_ss != tex2d_xy.y) clip(-1);
 
-	POBJ_PRE_CONTEXT;
+	//POBJ_PRE_CONTEXT;
+	if (g_cbClipInfo.clip_flag & 0x1)
+		clip(dot(g_cbClipInfo.vec_clipplane, input.f3PosWS - g_cbClipInfo.pos_clipplane) > 0 ? -1 : 1);
+	if (g_cbClipInfo.clip_flag & 0x2)
+		clip(!IsInsideClipBox(input.f3PosWS, g_cbClipInfo.mat_clipbox_ws2bs) ? -1 : 1);
+	clip(input.f4PosSS.z);
+	clip(input.f4PosSS.z / input.f4PosSS.w);
+	float3 pos_ip_ss = float3(input.f4PosSS.xy, 0.0f);
+	float3 pos_ip_ws = TransformPoint(pos_ip_ss, g_cbCamState.mat_ss2ws);
+	float3 vec_pos_ip2frag = input.f3PosWS - pos_ip_ws;
+	float z_depth = length(vec_pos_ip2frag) /*- g_cbPobj.depth_forward_bias*/;
+	clip(dot(vec_pos_ip2frag, g_cbCamState.dir_view_ws) < 0 ? -1 : 1);
+	float3 view_dir = g_cbCamState.dir_view_ws;
+	if (g_cbCamState.cam_flag & 0x1) {
+		view_dir = pos_ip_ws - g_cbCamState.pos_cam_ws;
+		z_depth += length(view_dir);
+	}
+	else {
+		z_depth += g_cbCamState.near_plane;
+	}
+	view_dir = normalize(view_dir);
+
+
 
 	// Atomically allocate space in the deep buffer
 	uint fc = 0;
