39907510f9ae39dfff67572fb9fc481d2813a121
korfriend <korfriend@gmail.com>
2023-11-19 04:10:01 +0900

    bugfix: crashes when otf is not set in curved slicer

--- CurvedSlicerVR.cpp
+++ CurvedSlicerVR.cpp
@@ -563,13 +563,12 @@ bool RenderVrCurvedSlicer(VmFnContainer* _fncontainer,
 			if (tobj_windowing)
 				tobj_otf = tobj_windowing;
 		}
-		MapTable* tmap_data = tobj_otf->GetObjParamPtr<MapTable>("_TableMap_OTF");
-
 		if (tobj_otf == NULL)
 		{
 			VMERRORMESSAGE("NOT ASSIGNED OTF");
 			continue;
 		}
+		MapTable* tmap_data = tobj_otf->GetObjParamPtr<MapTable>("_TableMap_OTF");
 
 		VmVObjectVolume* mask_vol_obj = (VmVObjectVolume*)actor->GetAssociateRes("MASKVOLUME");
 
