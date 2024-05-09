6f19a4d0f41572b748535e4642e8ad0a631808a0
korfriend <korfriend@gmail.com>
2024-05-10 02:46:59 +0900

    update migrated core

--- VimHelpers.h
+++ VimHelpers.h
@@ -22,6 +22,8 @@ typedef const glm::fmat4x4* const_f44p;
 //typedef const void* const_f3p;
 //typedef const void* const_f44p;
 
+#include <chrono>
+
 // ==> 나중에 void 로...
 
 #define __staticinline extern "C" __declspec(dllexport) inline
@@ -278,6 +280,51 @@ namespace vmhelpers {
 	* @remarks
 	*/
 	__staticinline void GetCPUInstructionInfo(int* cpu_info);
+
+	struct VmTimer
+	{
+		std::chrono::high_resolution_clock::time_point timestamp = std::chrono::high_resolution_clock::now();
+
+		// Record a reference timestamp
+		inline void record()
+		{
+			timestamp = std::chrono::high_resolution_clock::now();
+		}
+
+		// Elapsed time in seconds between the wi::Timer creation or last recording and "timestamp2"
+		inline double elapsed_seconds_since(std::chrono::high_resolution_clock::time_point timestamp2)
+		{
+			std::chrono::duration<double> time_span = std::chrono::duration_cast<std::chrono::duration<double>>(timestamp2 - timestamp);
+			return time_span.count();
+		}
+
+		// Elapsed time in seconds since the wi::Timer creation or last recording
+		inline double elapsed_seconds()
+		{
+			return elapsed_seconds_since(std::chrono::high_resolution_clock::now());
+		}
+
+		// Elapsed time in milliseconds since the wi::Timer creation or last recording
+		inline double elapsed_milliseconds()
+		{
+			return elapsed_seconds() * 1000.0;
+		}
+
+		// Elapsed time in milliseconds since the wi::Timer creation or last recording
+		inline double elapsed()
+		{
+			return elapsed_milliseconds();
+		}
+
+		// Record a reference timestamp and return elapsed time in seconds since the wi::Timer creation or last recording
+		inline double record_elapsed_seconds()
+		{
+			auto timestamp2 = std::chrono::high_resolution_clock::now();
+			auto elapsed = elapsed_seconds_since(timestamp2);
+			timestamp = timestamp2;
+			return elapsed;
+		}
+	};
 
 	/*!
 	 * @fn ullong vxhelpers::GetCurrentTimePack()
