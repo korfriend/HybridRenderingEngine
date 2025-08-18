5530b3da2d136593183b805c4c94c49716aceb31
korfriend <korfriend@gmail.com>
2025-08-18 11:29:03 -1000

    vzm core update

--- vzm2/Backlog.h
+++ vzm2/Backlog.h
@@ -39,6 +39,8 @@ namespace vz::backlog
 
 	extern "C" UTIL_EXPORT void postThreadSafe(const std::string& input, LogLevel level = LogLevel::Info);
 
+	extern "C" UTIL_EXPORT void enableLogDisplay(const bool consoleEnabled = true, const bool fileEnabled = true);
+
 	extern "C" UTIL_EXPORT void setLogLevel(LogLevel newLevel);
 
 	extern "C" UTIL_EXPORT LogLevel getLogLevel();
